3bfa02bb7aeadd7cd391d5f07c06ec22ea9b4088
Stanimir87 <103177983+Stanimir87@users.noreply.github.com>
2022-07-05 11:57:03 +0300

    fixed multiple HTTP (429) requests issue

--- src/components/episodeDetails/CharacterItem.js
+++ src/components/episodeDetails/CharacterItem.js
@@ -1,57 +1,27 @@
-import { Fragment, useCallback, useContext, useEffect, useState } from "react";
+import { Fragment } from "react";
 import {Link} from 'react-router-dom';
 import classes from "./CharacterItem.module.css";
-import LocationContext from "../../store/LocationContext";
-import CharLocationItem from "../locations/CharLocationItem";
+
+
 
 const CharacterItem = (props) => {
-  const [error, setError] = useState();
-  const [character, setCharacter] = useState();
-  const ctx = useContext(LocationContext);
-
-  const getCharacter = useCallback(async () => {
-    try {
-      const response = await fetch(props.url);
-
-      if (!response.ok) {
-        throw new Error("Something went wrong");
-      }
-      const data = await response.json();
-      setCharacter(data);
-      ctx.setLocation(data.location.url);
-      ctx.setOrigin(data.origin.url);
-    } catch {
-      setError(error.message);
-    }
-  }, []);
-
-  useEffect(() => {
-    getCharacter();
-  }, [getCharacter]);
-
-  if (!character) {
-    return <p>Loading...</p>;
-  }
-
-      // ctx.setLocation(character);
-      // ctx.setOrigin(character);
 
   return (
     <Fragment>
       <li className={classes.character}>
-        <div className={classes.details}><p><strong>Name: </strong>{character.name}
+        <div className={classes.details}><p><strong>Name: </strong>{props.name}
         <br />
-        <strong>Status:</strong> {character.status}
+        <strong>Status:</strong> {props.status}
         <br />
-        <strong>Species:</strong> {character.species}
+        <strong>Species:</strong> {props.species}
         <br />        
-        <strong>Origin:</strong><Link to={`/location/${character.origin.name}`}> {character.origin.name}</Link>
+        <strong>Origin:</strong><Link to={`/location/${props.origin.name}`}> {props.origin.name}</Link>
         <br />
-        <strong>Gender: </strong>{character.gender}
+        <strong>Gender: </strong>{props.gender}
         <br />
-        <strong>Location: </strong><Link to={`/location/${character.location.name}`}> {character.location.name}</Link>
+        <strong>Location: </strong><Link to={`/location/${props.location.name}`}> {props.location.name}</Link>
         </p></div>
-        <img src={character.image} alt='pic' />
+        <img src={props.img} alt='pic' />
       </li>
     </Fragment>
   );
--- src/components/episodeDetails/CharacterList.js
+++ src/components/episodeDetails/CharacterList.js
@@ -1,11 +1,55 @@
 import CharacterItem from "./CharacterItem";
 import classes from "./CharacterList.module.css";
+import { useCallback, useEffect, useState } from "react";
 
 const CharacterList = (props) => {
+  const [chars, setChars] = useState();
+  const [error, setError] = useState();
+  let charId = [];
+
+  for (let i of props.characters) {
+    charId.push(i.slice(42));
+  }
+
+  const getCharacter = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${charId}`
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      setChars(data);
+    } catch {
+      setError(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    getCharacter();
+  }, [getCharacter]);
+
+  if (!chars) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <ul className={classes["characters-list"]}>
-      {props.characters.map((character, index) => (
-        <CharacterItem key={index} url={character} />
+      {chars.map((char) => (
+        <CharacterItem
+          key={char.id}
+          id={char.id}
+          name={char.name}
+          status={char.status}
+          species={char.species}
+          gender={char.gender}
+          origin={char.origin}
+          location={char.location}
+          img={char.image}
+
+        />
       ))}
     </ul>
   );
